9ea4b0639bdffe35f6dc628dca9d3f12ba0ab7a5
Rohan Boutelle <105883535+rohanboutelle01@users.noreply.github.com>
2023-02-03 15:42:46 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "poop":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . e . . . . . . . 
+. . . . . . . e e e . . . . . . 
+. . . . . . e e e e e . . . . . 
+. . . . . e e e e e e e . . . . 
+. . . . e f 1 e e f 1 e e . . . 
+. . . e e 1 1 e e 1 1 e e e . . 
+. . e e e e e e e e e e e e e . 
+. . . e 1 1 1 1 1 1 1 1 1 e . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
